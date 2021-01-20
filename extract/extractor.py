a833cf9c44a411bd48f2f33665fb902e087f6d3e
jayr1125 <39975851+jayr1125@users.noreply.github.com>
2021-01-20 15:06:09 +0800

    Add files via upload

---
+++ extract/extractor.py
@@ -0,0 +1,35 @@
+#import the libraries
+import os
+import shutil
+from zipfile import ZipFile
+
+def file_extractor(filepath):
+    #create a list of filenames in the filepath
+    sources = os.listdir(filepath)
+    #removes the .zip extension from filenames
+    destinations = []
+    for source in sources:
+        destinations.append(source.replace('.zip',''))
+    #create a folder for every filename
+    for destination in destinations:
+        path = filepath + '\\' + destination
+        os.mkdir(path)
+    
+    #create a dictionary of sources and destinations
+    pts = dict(zip(sources,destinations))
+    for i,j in pts.items():
+        src = filepath + '\\' + i
+        dst = filepath + '\\' + j
+        #move the sources to the destinations
+        shutil.move(src,dst)
+        #change to destination directory
+        os.chdir(dst)
+        #open file as zip object
+        with ZipFile(i, 'r') as zipObj:
+            zipObj.extractall()
+            zipObj.close()
+        #delete the zip file after extraction
+        os.remove(i)
+        os.chdir(r'C:\Users\RodolfoSalvador\Desktop')
+        
+file_extractor(input("Input Filepath: "))
